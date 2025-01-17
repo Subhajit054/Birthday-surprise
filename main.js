d167b873dee00659f01978debc338fcb89ca5ff0
Subhajit054 <iam788494@gmail.com>
2025-01-17 20:56:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-// Register ScrollTrigger with GSAP
 gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => {
-  // Timeline for scroll animations
-  const tl = gsap.timeline({
+  gsap.timeline({
     scrollTrigger: {
-      trigger: ".wrapper", // Element to trigger the animations
-      start: "top top", // Start at the top of the viewport
-      end: "+=150%", // End point for scrolling
-      pin: true, // Pin the element during the animation
-      scrub: true, // Smooth scroll animations
-      markers: true, // Debugging markers (optional, remove for production)
+      trigger: ".wrapper",
+      start: "top top",
+      end: "+=150%",
+      pin: true,
+      scrub: true,
+      markers: true,
     },
-  });
-
-  // Scale animation for the hero section
-  tl.to(".section.hero", {
-    scale: 1.2,
-    z: 350,
-    transformOrigin: "center center",
-    ease: "power1.inOut",
-  });
+  }).to(".section.hero", { scale: 1.2, ease: "power1.inOut" });
 });
